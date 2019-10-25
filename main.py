c2c552e3e49ab1b81c98e0b2e6a9c73ba809cc3c
Boon Keong Lee <blee@moneylion.com>
2019-10-25 19:45:35 +0800

    Add logger info

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ from util.logger import log_setup
 
 
 def main():
-    print('Opening Chrome')
+    logger.info('Opening Chrome')
     # Download chromedriver from https://chromedriver.storage.googleapis.com/index.html?path=2.35/
     # Access the webdriver via its full path
     # browser = webdriver.Chrome('/Users/mavx/Downloads/chromedriver')
     browser = webdriver.Chrome()
-    print('Accessing Yahoo.com')
+    logger.info('Accessing Yahoo.com')
     browser.get('http://www.yahoo.com')
 
     time.sleep(2)
 
-    print('Finding search box')
+    logger.info('Finding search box')
     elem = browser.find_element_by_name('p')  # Find the search box
-    print('Searching for seleniumhq')
+    logger.info('Searching for seleniumhq')
     elem.send_keys('seleniumhq' + Keys.RETURN)
 
     # Randomising wait time to mimic humans!
     time.sleep(random.randint(0, 5))
 
-    print('Quitting browser')
+    logger.info('Quitting browser')
     browser.quit()
 
 
